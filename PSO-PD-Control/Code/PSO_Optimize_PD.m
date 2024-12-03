a09ba645ebb923aba6181d2b8274350b6b9e1bbb
AdharshAero <87041318+AdharshAero@users.noreply.github.com>
2024-12-03 00:59:28 -0700

    PSO Full Implementation

--- PSO-PD-Control/Code/PSO_Optimize_PD.m
+++ PSO-PD-Control/Code/PSO_Optimize_PD.m
@@ -1,7 +1,7 @@
-%Initialize the robotic arm
+% Initialize the robotic arm
 [robot, arm_length] = Robotic_arm_model();
 
-%Desired Trajectory
+% Desired Trajectory
 x_func = @(t) (35 + 25*cos(2*pi*t/10))/100;
 y_func = @(t) (35 + 25*sin(2*pi*t/10))/100;
 z_func = @(t) 90/100*t.^0;
@@ -14,13 +14,25 @@ qd_d_sym = diff(q_d_sym);
 q_d_func = matlabFunction(q_d_sym, 'Vars', t);
 qd_d_func = matlabFunction(qd_d_sym, 'Vars', t);
 
-t_span = [0 10]; % Time span for simulation
-initial_state = [30*pi/180; -30*pi/180; 80*pi/180; 0; 0; 0]; % Initial joint angles and velocities
-
-%Transformation Matrix
+% Transformation Matrix
 transform = Forward_Kinematics([arm_length(1) pi/2 0], [0 0 arm_length(2)], [0 0 arm_length(3)]);
+transform = matlabFunction(transform(1:3,4)', 'Vars', {'theta1', 'theta2', 'theta3'});
 
-%Algorithm Parameters
+% Robots Dynamics Matrices
+syms q1 q2 q3 qd1 qd2 qd3 real
+q_sym = [q1; q2; q3];
+qd_sym = [qd1; qd2; qd3];
+
+M_sym = robot.inertia(q_sym');
+C_sym = robot.coriolis(q_sym', qd_sym');
+G_sym = robot.gravload(q_sym');
+
+% Convert symbolic expressions to MATLAB functions
+M_func = matlabFunction(M_sym, 'Vars', {q1, q2, q3});
+C_func = matlabFunction(C_sym, 'Vars', {q1, q2, q3, qd1, qd2, qd3});
+G_func = matlabFunction(G_sym, 'Vars', {q1, q2, q3});
+
+% PSO Parameters
 Particle_size = 50;
 Iterations = 300;
 Dimensions = 6;
@@ -28,7 +40,7 @@ c1 = 1.5;
 c2 = 0.75;
 w = 0.9;
 
-%Range of values for Kp and Kd
+% Range of values for Kp and Kd
 Kp_range  =[0 1000];
 Kd_range  =[0 100];
 
@@ -36,25 +48,77 @@ Kd_range  =[0 100];
 Particles_Population = zeros(Particle_size, Dimensions);
 Particles_Velocity = zeros(Particle_size, Dimensions);
 
-%Randomly populate the population with number between respective ranges of Kp and Kd
+% Randomly populate with number between respective ranges of Kp and Kd
 Particles_Population(:,1:3) = Kp_range(1) - (Kp_range(1)-Kp_range(2)).*rand(Particle_size, Dimensions/2);
 Particles_Population(:,4:6) = Kd_range(1) - (Kd_range(1)-Kd_range(2)).*rand(Particle_size, Dimensions/2);
 Particles_Velocity = rand(size(Particles_Velocity));
+
+% Time span for simulation
+t_span = [0 10];
+initial_state = [30*pi/180; -30*pi/180; 80*pi/180; 0; 0; 0]; % Initial joint angles and velocities
+initial_state_all = repmat(initial_state, 1, Particle_size);
+
+% Initial Population Fitness
+[t, state] = ode15s(@(t, state) Robot_dynamics(t, state, q_d_func, qd_d_func, Particles_Population, M_func, C_func, G_func), t_span, initial_state_all(:));
+state = reshape(state, [], 6, Particle_size);
+Fitness = zeros(Particle_size,1);
+desired_EOF_pos = [x_func(t) y_func(t) z_func(t)];
+for i = 1:Particle_size
+    actual_end_effector = transform(state(:,1,i), state(:,2,i), state(:,3,i));
+    Fitness(i) = mean(sqrt(sum((desired_EOF_pos - actual_end_effector).^2,2)));
+end
+
+% Initializing Global and Particle for Initial Population
+PBest_Fitness = Fitness;
+PBest = Particles_Population;
+[GBest_Fitness, Loc] = min(Fitness);
+GBest = Particles_Population(Loc,:);
+
+GBest_history = zeros(26, 1);
+PBest_avg_history = zeros(26, 1);
+
+% Store initial values
+GBest_history(1) = GBest_Fitness;
+PBest_avg_history(1) = mean(PBest_Fitness);
+
 tic
-fitness = Fitness_Function(x_func, y_func, z_func, robot, arm_length, Particles_Population(1,:), transform(1:3,4));
+hold on
+for iter = 1:300
+    % Next Generation Iterations
+    % Create the next Population by computing the Velocity
+    Particles_Velocity = w * Particles_Velocity + c1 * rand(size(Particles_Velocity)) .* (PBest - Particles_Population) + c2 * rand(size(Particles_Velocity)) .* (GBest - Particles_Population);
+    Particles_Population = Particles_Population + Particles_Velocity;
+    % Ensure Kp and Kd values are positive
+    Particles_Population = max(Particles_Population, 5*rand);
+
+    [t, state] = ode15s(@(t, state) Robot_dynamics(t, state, q_d_func, qd_d_func, Particles_Population, M_func, C_func, G_func), t_span, initial_state_all(:));
+    state = reshape(state, [], 6, Particle_size);
+    Fitness = zeros(Particle_size,1);
+    desired_EOF_pos = [x_func(t) y_func(t) z_func(t)];
+    for i = 1:Particle_size
+        actual_end_effector = transform(state(:,1,i), state(:,2,i), state(:,3,i));
+        Fitness(i) = mean(sqrt(sum((desired_EOF_pos - actual_end_effector).^2,2)));
+    end
+
+    PBest(PBest_Fitness > Fitness, :) = Particles_Population(PBest_Fitness > Fitness, :);
+    PBest_Fitness(PBest_Fitness > Fitness) = Fitness(PBest_Fitness > Fitness);
+    [temp, Loc] = min(Fitness);
+    if temp < GBest_Fitness
+        GBest = Particles_Population(Loc,:);
+        GBest_Fitness = temp;
+    end
+    GBest_history(iter+1) = GBest_Fitness;
+    PBest_avg_history(iter+1) = mean(PBest_Fitness);
+end
 time = toc;
 
-function [fitness] = Fitness_Function(x_func, y_func, z_func, robot, arm_length, PD_Particle, transform)
-    % ODE45 call
-    [t, state] = ode15s(@(t, state) Robot_dynamics(t, state, robot, q_d_func, qd_d_func, PD_Particle), t_span, initial_state);
-    actual_end_effector = zeros(length(t),3);
-    for i = 1:length(state)
-        joint = state(i,1:3);
-        theta1 = joint(1);
-        theta2 = joint(2);
-        theta3 = joint(3);
-        actual_end_effector(i,:) = double(subs(transform))';
-    end
-    desired_EOF_pos = [x_func(t) y_func(t) z_func(t)];
-    fitness = mean(sqrt(sum((desired_EOF_pos - actual_end_effector).^2,2)));
-end
+num_iterations = 300;
+figure;
+plot(0:num_iterations, GBest_history, 'b-', 'LineWidth', 2);
+hold on;
+plot(0:num_iterations, PBest_avg_history, 'r--', 'LineWidth', 1.5);
+xlabel('Iteration');
+ylabel('RMS Error');
+title('PSO Convergence for PD Controller Optimization');
+legend('Global Best Fitness', 'Particles Mean Fitness');
+grid on;
--- PSO-PD-Control/Code/Robot_dynamics.m
+++ PSO-PD-Control/Code/Robot_dynamics.m
@@ -1,23 +1,33 @@
-function dstate = Robot_dynamics(t, state, robot, q_d_func, qd_d_func, PD_Particle)
-    q = state(1:3);
-    qd = state(4:6);
-
-    % Compute desired trajectory
+function dstate = Robot_dynamics(t, state, q_d_func, qd_d_func, PD_Particle, M_Matrix, C_Matrix, G_Matrix)
+    num_particles = size(PD_Particle, 1);
+    state = reshape(state, 6, num_particles);
+    
+    % Compute desired trajectory (same for all particles)
     q_d = q_d_func(t);
     qd_d = qd_d_func(t);
 
-    % Compute control input (PD control)
-    Kp = diag(PD_Particle(1,1:3)); % Proportional gains
-    Kd = diag(PD_Particle(1,4:6));    % Derivative gains
+    % Vectorized operations
+    q = state(1:3, :);
+    qd = state(4:6, :);
     e = q_d - q;
     ed = qd_d - qd;
-    tau = Kp*e + Kd*ed;
-    
-    M = robot.inertia(q');
-    C = robot.coriolis(q', qd');
-    G = robot.gravload(q');
-    qdd = M\(tau - C*qd - G');
-    
-    % Return state derivative
+
+    % Pre-allocate arrays
+    qdd = zeros(3, num_particles);
+
+    % Vectorized PD control
+    Kp = reshape(PD_Particle(:,1:3)', 3, num_particles);
+    Kd = reshape(PD_Particle(:,4:6)', 3, num_particles);
+    tau = Kp .* e + Kd .* ed;
+
+    % Compute dynamics (this part might be challenging to fully vectorize)
+    for i = 1:num_particles
+        M = M_Matrix(q(1,i), q(2,i), q(3,i));
+        C = C_Matrix(q(1,i), q(2,i), q(3,i), qd(1,i), qd(2,i), qd(3,i));
+        G = G_Matrix(q(1,i), q(2,i), q(3,i));
+        qdd(:,i) = M \ (tau(:,i) - C*qd(:,i) - G');
+    end
+
     dstate = [qd; qdd];
+    dstate = dstate(:);
 end
