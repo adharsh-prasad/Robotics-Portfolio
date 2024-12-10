3e5a5a84974d3309e1f3cab9990a0b10826b5f4f
AdharshAero <87041318+AdharshAero@users.noreply.github.com>
2024-12-10 16:12:02 -0700

    Updating Analysis plot file

--- PD-Control-Robotic-Arm/Code/Analysis_Plots.m
+++ PD-Control-Robotic-Arm/Code/Analysis_Plots.m
@@ -1,20 +1,20 @@
-close all
-[robot, arm_length] = Robotic_arm_model();
-
+% close all
+% [robot, arm_length] = Robotic_arm_model();
+% 
 x_func = @(t) (35 + 25*cos(2*pi*t/10))/100;
 y_func = @(t) (35 + 25*sin(2*pi*t/10))/100;
 z_func = @(t) 90/100*t.^0;
-
-[T, joint_angles] = Joint_trajectory(x_func, y_func, z_func, robot, arm_length);
-tranform = Forward_Kinematics([arm_length(1) pi/2 0], [0 0 arm_length(2)], [0 0 arm_length(3)]);
-end_effector = [];
-for i = 1:length(joint_angles)
-    joint = joint_angles(i,1:3);
-    theta1 = joint(1);
-    theta2 = joint(2);
-    theta3 = joint(3);
-    end_effector = [end_effector;double(subs(tranform(1:3,4)))'];
-end
+% 
+% [T, joint_angles] = Joint_trajectory(x_func, y_func, z_func, robot, arm_length);
+% tranform = Forward_Kinematics([arm_length(1) pi/2 0], [0 0 arm_length(2)], [0 0 arm_length(3)]);
+% end_effector = [];
+% for i = 1:length(joint_angles)
+%     joint = joint_angles(i,1:3);
+%     theta1 = joint(1);
+%     theta2 = joint(2);
+%     theta3 = joint(3);
+%     end_effector = [end_effector;double(subs(tranform(1:3,4)))'];
+% end
 figure('Position', get(0, 'Screensize'));  % Create larger figure window
 % Plot desired trajectory
 plot3(x_func(T), y_func(T), z_func(T), 'b-', 'LineWidth', 2);
@@ -23,7 +23,7 @@ hold on
 % Plot actual end-effector trajectory
 scatter3(end_effector(:,1), end_effector(:,2), end_effector(:,3), 20, 'r.', 'MarkerFaceAlpha', 0.6);
 
-Enhance plot appearance
+%Enhance plot appearance
 grid on
 box on
 axis equal
@@ -44,4 +44,3 @@ ax.XLim = [0 1];
 ax.YLim = [0 1];
 ax.ZLim = [0 1];
 ax.LineWidth = 1.5;
-
